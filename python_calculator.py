7e373b5c832d4d6e8156b8bfadfef95b07ad01fe
Kenny Vu <Kenny Vu>
2016-07-18 19:47:17 +0100

    Changelog: Added GUI

--- python_calculator.py
+++ python_calculator.py
@@ -1,4 +1,12 @@
+from tkinter import *
 import sys
+
+def num_press(num):
+    print(num)
+
+def clear():
+    text.delete(0,END)
+    return
 
 def getNum(num):
     print("What is the %s number?" % (num))
@@ -7,15 +15,38 @@ def getNum(num):
 
 def add(n1,n2):
     print("%i + %i =" %(n1,n2), (n1+n2))
-
 def subtract(n1,n2):
     print("%i - %i =" %(n1,n2), (n1-n2))
-
 def multiply(n1,n2):
     print("%i * %i =" %(n1,n2), (n1*n2))
-
 def divide(n1,n2):
     print("%i / %i =" %(n1,n2), (n1/n2))
+
+root = Tk()
+root.title("Calculator")
+frame =Frame(root)
+frame.grid()
+
+text = Entry(frame,bd=20, insertwidth=1,font=30)
+#text.config(state=DISABLED)
+text.grid(row = 0, column = 0, columnspan = 5)
+
+buttons = ["1","2","3","(",")",
+           "4","5","6","x","/",
+           "7","8","9","+","-",
+           ".","0","CE","C","="]
+i=0
+
+button = []
+
+for rows in range (1,5):
+    for columns in range(5):
+       button.append(Button(frame,text=buttons[i],padx=16,pady=16,bd=6,fg="black",command=lambda x=i: num_press(buttons[x])))
+       button[i].grid(row = rows, column = columns)
+       i+=1
+
+
+root.mainloop()
 
 menu='''
 (1)ADD
@@ -25,7 +56,9 @@ menu='''
 (5)EXIT
 '''
 
+'''
 print(menu)
+
 
 #input=int(sys.stdin.readline())
 input=input("Choose an option:")
@@ -49,3 +82,4 @@ elif input == "4":
 elif input == "5":
     print("Closing calculator")
     sys.exit
+'''
