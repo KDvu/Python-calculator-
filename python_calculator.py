d746a8a510cf8f94fa19675c06ad15d770ccf455
Kenny Vu <Kenny Vu>
2016-07-18 15:30:49 +0100

    Changelog:Small change to how input is taken

--- python_calculator.py
+++ python_calculator.py
@@ -17,7 +17,7 @@ def multiply(n1,n2):
 def divide(n1,n2):
     print("%i / %i =" %(n1,n2), (n1/n2))
 
-menu='''Choose an option:
+menu='''
 (1)ADD
 (2)SUBTRACT
 (3)MULTIPLY
@@ -27,24 +27,25 @@ menu='''Choose an option:
 
 print(menu)
 
-input=int(sys.stdin.readline())
+#input=int(sys.stdin.readline())
+input=input("Choose an option:")
 
-if input == 1:
+if input == "1":
     num1 = getNum("first")
     num2 = getNum("second")
     add(num1,num2)
-elif input == 2:
+elif input == "2":
     num1 = getNum("first")
     num2 = getNum("second")
     subtract(num1,num2)
-elif input == 3:
+elif input == "3":
     num1 = getNum("first")
     num2 = getNum("second")
     multiply(num1,num2)
-elif input == 4:
+elif input == "4":
     num1 = getNum("first")
     num2 = getNum("second")
     divide(num1,num2)
-elif input == 5:
+elif input == "5":
     print("Closing calculator")
     sys.exit
