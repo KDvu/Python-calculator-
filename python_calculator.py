bacaaca73fc4ae39cef9d239389034893f770e17
Kenny Vu <Kenny Vu>
2016-07-20 19:19:15 +0100

    Changelog: Removed some comments and test code

--- python_calculator.py
+++ python_calculator.py
@@ -26,10 +26,8 @@ class Calculator():
                     self.new_num = False
                     display.insert(self.current_no,key)
                 self.current = float(display.get())
-                print(self.current)
         except ValueError:
             if key == "+" or key == "-" or key == "*" or key == "/":
-                print("Operation: ", key)
                 self.operation(key)
             elif key == ".":
                 self.current_no+=1
@@ -40,7 +38,6 @@ class Calculator():
                 display.insert(self.current_no,key)
             elif key == "=":
                 self.calculateTotal()
-                print("equals")
             elif key == "C":
                 self.clear()
             elif key == "CE":
@@ -134,39 +131,3 @@ for rows in range (1,5):
 
 
 root.mainloop()
-
-menu='''
-(1)ADD
-(2)SUBTRACT
-(3)MULTIPLY
-(4)DIVIDE
-(5)EXIT
-'''
-
-'''
-print(menu)
-
-
-#input=int(sys.stdin.readline())
-input=input("Choose an option:")
-
-if input == "1":
-    num1 = getNum("first")
-    num2 = getNum("second")
-    add(num1,num2)
-elif input == "2":
-    num1 = getNum("first")
-    num2 = getNum("second")
-    subtract(num1,num2)
-elif input == "3":
-    num1 = getNum("first")
-    num2 = getNum("second")
-    multiply(num1,num2)
-elif input == "4":
-    num1 = getNum("first")
-    num2 = getNum("second")
-    divide(num1,num2)
-elif input == "5":
-    print("Closing calculator")
-    sys.exit
-    '''
